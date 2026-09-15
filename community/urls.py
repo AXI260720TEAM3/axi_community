b5f04ca0754932b783293e01bf4675d4dc84edb3
ljh4git3-test <ljh4git3@gmail.com>
2026-09-15 15:36:04 +0900

    프로젝트 모집 작성자는 본인 글 삭제가능

--- community/urls.py
+++ community/urls.py
@@ -53,6 +53,7 @@ urlpatterns = [
     path('recruit/<int:recruit_id>/close/', recruit.recruit_close, name='recruit_close'),
     path('recruit/application/<int:app_id>/<str:status>/', recruit.recruit_application_decide, name='recruit_app_decide'),
     path('recruit/<int:recruit_id>/cancel/', recruit.recruit_cancel, name='recruit_cancel'),
+    path('recruit/<int:recruit_id>/delete/', recruit.recruit_delete, name='recruit_delete'),
 
     # ---------------------------------------------------------------- [C] 댓글
     path("posts/<int:post_id>/comments/", comment.comment_create, name="comment_create"),
